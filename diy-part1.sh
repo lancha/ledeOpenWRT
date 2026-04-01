c6ed797b8af1ba5b0283a4952acd55b657eef7da
MikeLan <binglancha@gmail.com>
2026-04-01 19:49:31 +0800

    Add new feed sources for packages and routing
    Added multiple new feed sources to feeds.conf.default.

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,9 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
+echo 'src-git routing https://github.com/coolsnowwolf/routing' >>feeds.conf.default
+echo 'src-git telephony https://github.com/coolsnowwolf/telephony.git' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
